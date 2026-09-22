82e6cc566c456309148034cde82b36f2a8487fb6
LPA <p.luchshev@khai.edu>
2026-09-22 08:50:48 +0300

    Project with OpenGL

---
+++ Examples/Clock/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
